05809423c79918e12e44b178447dd8c66579e658
Anders Brostrøm <80405973+AndersBros@users.noreply.github.com>
2025-05-16 11:46:33 +0200

    Create __init__.py

---
+++ src/aerosoltools/loaders/__init__.py
@@ -0,0 +1,17 @@
+from .CPC import Load_CPC_file
+from .Discmini import Load_DiSCmini_file
+from .ELPI import Load_ELPI_file
+from .NS import Load_NS_file
+from .OPS import Load_OPS_file
+from .Partector import Load_Partector_file
+from .common import Load_data_from_folder
+
+__all__ = [
+    "Load_CPC_file",
+    "Load_DiSCmini_file",
+    "Load_ELPI_file",
+    "Load_NS_file",
+    "Load_OPS_file",
+    "Load_Partector_file",
+    "Load_data_from_folder",
+]
