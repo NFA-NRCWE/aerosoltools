879debdf0df359c368f8b3a0f0988242c43e3f6f
Anders Brostrøm <80405973+AndersBros@users.noreply.github.com>
2025-05-17 07:39:24 +0200

    Update Discmini.py

--- src/aerosoltools/loaders/Discmini.py
+++ src/aerosoltools/loaders/Discmini.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 from .Common import detect_delimiter
-from .aerosolalt import AerosolAlt
+from ..aerosolalt import AerosolAlt
 
 ###############################################################################
 
